f387d09e6acef0fdf74df63ae5ed7ecdf9f57827
withjak <akshbishnoi@gmail.com>
2024-06-11 09:06:14 +0530

    added snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akshay/inspector "1.1.0"
+(defproject org.clojars.akshay/inspector "1.1.0-SNAPSHOT"
   :description "See when your functions are called. See function call hierarchy, db data used when a particular function is called, and export data"
   :url "https://github.com/withjak/inspector"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
