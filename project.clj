eb18df9a5e61c1e9cccd24a1bc77591ca77f451b
withjak <akshbishnoi@gmail.com>
2024-06-11 14:50:27 +0530

    removed snapshot

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject org.clojars.akshay/inspector "1.1.0-SNAPSHOT"
+(defproject org.clojars.akshay/inspector "1.1.0"
   :description "See when your functions are called. See function call hierarchy, db data used when a particular function is called, and export data"
   :url "https://github.com/withjak/inspector"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org/"
+                                    :creds :gpg}]]
   :repl-options {:init-ns inspector.core})
